a82ee1e7f12350a9a86a22be549873eba2b24665
mirkan1 <mirkanbaba1@gmail.com>
2021-08-21 18:32:11 -0400

    test_jsonPairDataFrame

--- tests/test_client.py
+++ tests/test_client.py
@@ -5,44 +5,51 @@ df_type = type(pd.DataFrame())
 safe_graph = client.HTTP_Client("x8TUW4IV3hYC1L4Xav56nChUWwtBisRY")
 
 def test_place():
+    # [random.choice(list(safe_graph.__pattern__.values())) for i in range(random.randint(10)) if i not in ["__header__", "__footer__"]]
     placekey = "224-222@5vg-7gv-d7q"
-    assert type(safe_graph.place(placekey, dataset= ["*"], return_type="pandas")) == df_type
-    assert type(safe_graph.place(placekey, dataset= ["*"], return_type="list")) == list
+    assert type(safe_graph.place(placekey, columns=["*"], return_type="pandas")) == df_type
+    assert type(safe_graph.place(placekey, columns=["*"], return_type="list")) == list
 
-    assert type(safe_graph.place(placekey, dataset=["safegraph_core"], return_type="pandas")) == df_type
-    assert type(safe_graph.place(placekey, dataset=["safegraph_core"], return_type="list")) == list
-    assert type(safe_graph.place(placekey, dataset=["safegraph_geometry"], return_type="pandas")) == df_type
-    assert type(safe_graph.place(placekey, dataset=["safegraph_geometry"], return_type="list")) == list
-    assert type(safe_graph.place(placekey, dataset=["safegraph_patterns"], return_type="pandas")) == df_type
-    assert type(safe_graph.place(placekey, dataset=["safegraph_patterns"], return_type="list")) == list
+    assert type(safe_graph.place(placekey, columns=["*"], return_type="pandas")) == df_type
+    assert type(safe_graph.place(placekey, columns=["*"], return_type="list")) == list
+    assert type(safe_graph.place(placekey, columns=["*"], return_type="pandas")) == df_type
+    assert type(safe_graph.place(placekey, columns=["*"], return_type="list")) == list
+    assert type(safe_graph.place(placekey, columns=["*"], return_type="pandas")) == df_type
+    assert type(safe_graph.place(placekey, columns=["*"], return_type="list")) == list
 
-    assert type(safe_graph.place(placekey, dataset=["safegraph_core", "safegraph_patterns"], return_type="pandas")) == df_type
-    assert type(safe_graph.place(placekey, dataset=["safegraph_core", "safegraph_geometry"], return_type="list")) == list
+    assert type(safe_graph.place(placekey, columns=["*"], return_type="pandas")) == df_type
+    assert type(safe_graph.place(placekey, columns=["*"], return_type="list")) == list
 
-    assert type(safe_graph.place(placekey, dataset=["safegraph_geometry", "safegraph_core"], return_type="pandas")) == df_type
-    assert type(safe_graph.place(placekey, dataset=["safegraph_geometry", "safegraph_patterns"], return_type="list")) == list
+    assert type(safe_graph.place(placekey, columns=["*"], return_type="pandas")) == df_type
+    assert type(safe_graph.place(placekey, columns=["*"], return_type="list")) == list
     
-    assert type(safe_graph.place(placekey, dataset=["safegraph_core", "safegraph_geometry", "safegraph_patterns"], return_type="list")) == list
+    assert type(safe_graph.place(placekey, columns=["*"], return_type="list")) == list
 
 def test_places():
-    placekeys = ["224-222@5vg-7gv-d7q", "222-222@5qw-shj-7qz", "222-222@5s6-pyc-7qz", "zzy-222@5xc-k8q-zmk"]
-    assert type(safe_graph.places(placekeys, dataset= ["*"], return_type="pandas")) == df_type
-    assert type(safe_graph.places(placekeys, dataset= ["*"], return_type="list")) == list
+    placekeys = ["224-222@5vg-7gv-d7q", "222-222@5qw-shj-7qz", "222-222@5s6-pyc-7qz", "zzy-222@5xc-k8q-zmk"]   
+    assert type(safe_graph.places(placekeys, columns=["*"], return_type="pandas")) == df_type
+    assert type(safe_graph.places(placekeys, columns=["*"], return_type="list")) == list
     
-    assert type(safe_graph.places(placekeys, dataset=["safegraph_core"], return_type="pandas")) == df_type
-    assert type(safe_graph.places(placekeys, dataset=["safegraph_core"], return_type="list")) == list
-    assert type(safe_graph.places(placekeys, dataset=["safegraph_geometry"], return_type="pandas")) == df_type
-    assert type(safe_graph.places(placekeys, dataset=["safegraph_geometry"], return_type="list")) == list
-    assert type(safe_graph.places(placekeys, dataset=["safegraph_patterns"], return_type="pandas")) == df_type
-    assert type(safe_graph.places(placekeys, dataset=["safegraph_patterns"], return_type="list")) == list
+    assert type(safe_graph.places(placekeys, columns=["*"], return_type="pandas")) == df_type
+    assert type(safe_graph.places(placekeys, columns=["*"], return_type="list")) == list
+    assert type(safe_graph.places(placekeys, columns=["*"], return_type="pandas")) == df_type
+    assert type(safe_graph.places(placekeys, columns=["*"], return_type="list")) == list
+    assert type(safe_graph.places(placekeys, columns=["*"], return_type="pandas")) == df_type
+    assert type(safe_graph.places(placekeys, columns=["*"], return_type="list")) == list
 
-    assert type(safe_graph.places(placekeys, dataset=["safegraph_core", "safegraph_patterns"], return_type="pandas")) == df_type
-    assert type(safe_graph.places(placekeys, dataset=["safegraph_core", "safegraph_geometry"], return_type="list")) == list
+    assert type(safe_graph.places(placekeys, columns=["*"], return_type="pandas")) == df_type
+    assert type(safe_graph.places(placekeys, columns=["*"], return_type="list")) == list
 
-    assert type(safe_graph.places(placekeys, dataset=["safegraph_geometry", "safegraph_core"], return_type="pandas")) == df_type
-    assert type(safe_graph.places(placekeys, dataset=["safegraph_geometry", "safegraph_patterns"], return_type="list")) == list
+    assert type(safe_graph.places(placekeys, columns=["*"], return_type="pandas")) == df_type
+    assert type(safe_graph.places(placekeys, columns=["*"], return_type="list")) == list
     
-    assert type(safe_graph.places(placekeys, dataset=["safegraph_core", "safegraph_geometry", "safegraph_patterns"], return_type="list")) == list
+    assert type(safe_graph.places(placekeys, columns=["*"], return_type="list")) == list
+
+def test_jsonPairDataFrame():
+    placekeys = ["224-222@5vg-7gv-d7q", "222-222@5qw-shj-7qz", "222-222@5s6-pyc-7qz", "zzy-222@5xc-k8q-zmk"]   
+    df = safe_graph.places(placekeys, columns=["*"], return_type="pandas")
+    pure_data = df[["device_type", "placekey"]].dropna().values.tolist()
+    # pd.DataFrame(list(chain.from_iterable(df["device_type"].dropna().to_list())))
 
 def test_get_place_by_locatian_name_address():
     assert type(safe_graph.place_by_name(
