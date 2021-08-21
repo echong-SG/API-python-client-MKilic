363d63c95ec3ba6c3d7e4b14a15aca131f10daae
mirkan1 <mirkanbaba1@gmail.com>
2021-08-21 17:49:29 -0400

    new shema: __pattern__

--- safegraph_ql/client.py
+++ safegraph_ql/client.py
@@ -24,38 +24,139 @@ class HTTP_Client:
         self.__pattern__ = {
             "safegraph_core": { 
                 "__header__": "safegraph_core {",
-                "location_name": "location_name", 
-                "top_category": "top_category", 
-                "street_address": "street_address", 
-                "city": "city", 
-                "region": "region", 
-                "postal_code": "postal_code", 
-                "latitude": "latitude", 
-                "longitude": "longitude", 
-                "iso_country_code": "iso_country_code", 
+                "placekey" : "placekey",
+                "parent_placekey" : "parent_placekey",
+                "location_name" : "location_name",
+                "safegraph_brand_ids" : "safegraph_brand_ids",
+                "brands" : """
+                    brands {
+                        brand_id: brand_id,
+                        brand_name: brand_name,
+                    }
+                """,
+                "top_category" : "top_category",
+                "sub_category" : "sub_category",
+                "naics_code" : "naics_code",
+                "latitude" : "latitude",
+                "longitude" : "longitude",
+                "street_address" : "street_address",
+                "city" : "city",
+                "region" : "region",
+                "postal_code" : "postal_code",
+                "iso_country_code" : "iso_country_code",
+                "phone_number" : "phone_number",
+                "open_hours" : "open_hours",
+                "category_tags" : "category_tags",
+                "opened_on" : "opened_on",
+                "closed_on" : "closed_on",
+                "tracking_closed_since" : "tracking_closed_since",
+                "geometry_type" : "geometry_type",
                 "__footer__": "}"
             },
             "safegraph_geometry": {
                 "__header__": "safegraph_geometry {",
-                "location_name": "location_name", 
-                "street_address": "street_address", 
-                "city": "city", 
-                "region": "region", 
-                "postal_code": "postal_code", 
-                "latitude": "latitude", 
-                "longitude": "longitude", 
-                "polygon_wkt": "polygon_wkt", 
+                "placekey": "placekey",
+                "parent_placekey": "parent_placekey",
+                "location_name": "location_name",
+                "brands" : """
+                    brands {
+                        brand_id: brand_id,
+                        brand_name: brand_name,
+                    }
+                """,
+                "latitude": "latitude",
+                "longitude": "longitude",
+                "street_address": "street_address",
+                "city": "city",
+                "region": "region",
+                "postal_code": "postal_code",
+                "iso_country_code": "iso_country_code",
+                "polygon_wkt": "polygon_wkt",
+                "polygon_class": "polygon_class",
+                "includes_parking_lot": "includes_parking_lot",
+                "is_synthetic": "is_synthetic",
+                "building_height": "building_height",
+                "enclosed": "enclosed",
                 "__footer__": "}"
             },
             "safegraph_patterns": {
                 "__header__": "safegraph_patterns {",
+                "placekey": "placekey",
+                "parent_placekey": "parent_placekey",
+                "location_name": "location_name",
+                "street_address": "street_address",
+                "city": "city",
+                "region": "region",
+                "postal_code": "postal_code",
+                "safegraph_brand_ids": "safegraph_brand_ids",
+                "brands" : """
+                    brands {
+                        brand_id: brand_id,
+                        brand_name: brand_name,
+                    }
+                """,
                 "date_range_start": "date_range_start",
                 "date_range_end": "date_range_end",
-                "median_dwell": "median_dwell",
-                "bucketed_dwell_times": "bucketed_dwell_times { key value }",
-                "popularity_by_day": "popularity_by_day { key value }",
+                "raw_visit_counts": "raw_visit_counts",
+                "raw_visitor_counts": "raw_visitor_counts",
                 "visits_by_day": "visits_by_day",
-                "poi_cbg": "poi_cbg",
+                "visitor_home_cbgs": """
+                    visitor_home_cbgs {
+                        key: key
+                        value: value
+                        }
+                """,
+                "visitor_home_aggregation": """
+                    visitor_home_aggregation {
+                        key: key
+                        value: value
+                    }
+                """,
+                "visitor_daytime_cbgs": """
+                    visitor_daytime_cbgs {
+                        key: key
+                        value: value
+                    }
+                """,
+                "visitor_country_of_origin": """
+                    visitor_country_of_origin {
+                        key: key
+                        value: value
+                    }
+                """,
+                "distance_from_home": "distance_from_home",
+                "median_dwell": "median_dwell",
+                "bucketed_dwell_times": """
+                    bucketed_dwell_times {
+                        key: key
+                        value: value
+                    }
+                """,
+                "related_same_day_brand": """
+                    related_same_day_brand {
+                        key: key
+                        value: value
+                    }
+                """,
+                "related_same_month_brand": """
+                    related_same_month_brand {
+                        key: key
+                        value: value
+                    }
+                """,
+                "popularity_by_hour": "popularity_by_hour",
+                "popularity_by_day": """
+                    popularity_by_day {
+                        key: key
+                        value: value
+                    }
+                """,
+                "device_type": """
+                    device_type {
+                        key: key
+                        value: value
+                    }
+                """,
                 "__footer__": "}"
             }
         }
@@ -146,7 +247,7 @@ class HTTP_Client:
             for j in data_type:
                 dict_.update(place[j])
             data_frame.append(dict_)
-            
+
         if return_type == "pandas":
             df = pd.DataFrame(data_frame)
             return df
